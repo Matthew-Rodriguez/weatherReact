41966be03da4960e52c9dd3cd3040f0cad4ef947
Matthew James Rodriguez <mrodvusd36@gmail.com>
2020-02-06 16:40:57 -0800

    Updates

---
+++ precache-manifest.db2edf1edf1a295d30a3a6c727d4a31b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c22a302a16020172478d8c43a48a5c8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "86adc2864c5e6127282a",
+    "url": "/my-app/static/css/main.5cdd6407.chunk.css"
+  },
+  {
+    "revision": "013914e61c074d7fc6cf",
+    "url": "/my-app/static/js/2.377fad85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.377fad85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86adc2864c5e6127282a",
+    "url": "/my-app/static/js/main.8aaa2d74.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "d3cee4c0059d6b81401ec3734957438f",
+    "url": "/my-app/static/media/cloudy.d3cee4c0.jpg"
+  },
+  {
+    "revision": "df0cc5151d364d6c5431ea0f771ac3ec",
+    "url": "/my-app/static/media/night.df0cc515.png"
+  }
+]);
